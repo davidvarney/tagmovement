1742e4d246d012baca1129d760f2833f40b49186
David Varney <davidvarney@gmail.com>
2016-02-16 12:30:41 -0500

    Makes the contact email templates a little more friendly for viewing within one's email client.

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,49 +1,62 @@
 @extends('emails.email_basic')
 
 @section('content')
-    <table class="body-wrap">
-        <tr>
-            <td class="container">
+    <table class="body-wrap" style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; width: 100% !important; height: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; background: #efefef; margin: 0; padding: 0;" bgcolor="#efefef">
+        <tr style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">
+            <td class="container" style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; display: block !important; clear: both !important; max-width: 580px !important; margin: 0 auto; padding: 0;">
 
                 <!-- Message start -->
-                <table>
-                    <tr>
-                        <td align="center" class="masthead">
+                <table style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; width: 100% !important; border-collapse: collapse; margin: 0; padding: 0;">
+                    <tr style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">
+                        <td align="center" class="masthead" style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; color: white; background: #000; margin: 0; padding: 80px 0;" bgcolor="#000">
 
-                            <h1>T<span style="color:#A91F23;">A</span>G Movement</h1>
+                            <h1 style="font-size: 32px; font-family: 'open sans',Arial,sans-serif; line-height: 1.25; max-width: 90%; text-transform: uppercase; margin: 0 auto; padding: 0;">T<span style="color: #A91F23; font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">A</span>G Movement</h1>
 
                         </td>
                     </tr>
-                    <tr>
-                        <td class="content">
+                    <tr style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">
+                        <td class="content" style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; background: white; margin: 0; padding: 30px 35px;" bgcolor="white">
 
-                            <p>{!! $data['message'] !!}</p>
+                            <p style="font-size: 16px; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; font-weight: normal; margin: 0 0 20px; padding: 0;">
+                                {!! $data['message'] !!}
+                            </p>
 
-                            <p>
-                                <em>– {!! $data['name'] !!}</em><br>
-                                <em>Email - {!! $data['email'] !!}</em><br>
-                                <em>Phone - {!! $data['phone'] !!}</em>
+                            <p style="font-size: 16px; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; font-weight: normal; margin: 0 0 20px; padding: 0;">
+                                <em style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">
+                                    – {!! $data['name'] !!}</em>
+                                    <br style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;" />
+                                    <em style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">
+                                        Email - {!! $data['email'] !!}
+                                    </em>
+                                    <br style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;" />
+                                    <em style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">
+                                        Phone - {!! $data['phone'] !!}
+                                    </em>
                             </p>
 
                         </td>
                     </tr>
                 </table>
-
             </td>
         </tr>
-        <tr>
-            <td class="container">
+        <tr style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">
+            <td class="container" style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; display: block !important; clear: both !important; max-width: 580px !important; margin: 0 auto; padding: 0;">
 
                 <!-- Message start -->
-                <table>
-                    <tr>
-                        <td class="content footer" align="center">
-                            <p>Sent by T<span style="color:#A91F23;">A</span>G Movement, 4480 Refugee Rd, Ste. 301, Columbus, OH, 43232</p>
-                            <p><a href="mailto:admin@tagmovement.org">admin@tagmovement.org</a></p>
+                <table style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; width: 100% !important; border-collapse: collapse; margin: 0; padding: 0;">
+                    <tr style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">
+                        <td class="content footer" align="center" style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; background: white none; margin: 0; padding: 30px 35px;" bgcolor="white">
+                            <p style="font-size: 14px; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; font-weight: normal; color: #888; text-align: center; margin: 0; padding: 0;" align="center">
+                                Sent by T<span style="color: #A91F23; font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">A</span>G Movement, 4480 Refugee Rd, Ste. 301, Columbus, OH, 43232
+                            </p>
+                            <p style="font-size: 14px; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; font-weight: normal; color: #888; text-align: center; margin: 0; padding: 0;" align="center">
+                                <a href="mailto:admin@tagmovement.org" style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; color: #888; text-decoration: none; font-weight: bold; margin: 0; padding: 0;">
+                                    admin@tagmovement.org
+                                </a>
+                            </p>
                         </td>
                     </tr>
                 </table>
-
             </td>
         </tr>
     </table>
--- resources/views/emails/email_basic.blade.php
+++ resources/views/emails/email_basic.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; margin: 0; padding: 0;">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width"/>
 
     @include('emails.email_basic_css')
 </head>
-<body>
+<body style="font-size: 100%; font-family: 'open sans',Arial,sans-serif; line-height: 1.65; width: 100% !important; height: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; background: #efefef; margin: 0; padding: 0;" bgcolor="#efefef">
     @yield('content')
 </body>
 </html>
