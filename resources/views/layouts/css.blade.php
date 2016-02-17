f694830d4e1981051f0ba23282c4963e07622d6b
David Varney <davidvarney@gmail.com>
2016-02-17 15:22:57 -0500

    Adds a different Google Font to the project.

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -6,4 +6,5 @@
 
 <!-- Custom Fonts -->
 <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<!--<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>-->
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
