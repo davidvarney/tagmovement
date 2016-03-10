520f17e42da2578e405520a97574c65060dcebd5
David Varney <davidvarney@gmail.com>
2016-03-10 13:45:10 -0500

    Adds a more grid layouts for the admin flashes.

--- resources/views/layouts/admin_app.blade.php
+++ resources/views/layouts/admin_app.blade.php
@@ -105,7 +105,7 @@
     </nav>
 
     <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
             @if (Session::has('flash_notification.message'))
                 <div class="alert alert-{{ Session::get('flash_notification.level') }}">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
